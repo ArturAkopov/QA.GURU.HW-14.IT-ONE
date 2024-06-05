a725879bd96aefee8b2a8faa3f1da38052fce121
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-06-05 20:05:49 +0300

    Сформированы тесты и классы

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,5 +1,71 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.href;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.currentFrameUrl;
+
 public class MainPage {
 
+    private final SelenideElement
+            headerLogo = $(".header__logo-link"),
+            burgerButton = $(".burger-button"),
+            menuNavigation = $(".menu__nav"),
+            allProvider = $(".section").$(byText("Все услуги")),
+            bigHeading = $(".big-heading"),
+            allVacancies = $("#job-section").$(byText("Все вакансии"));
+
+    @Step("Открытие страницы \"https://www.it-one.ru/\"")
+    public MainPage openPage() {
+        open("");
+        return this;
+    }
+
+    @Step("Открытие бургер-меню")
+    public MainPage openBurgerMenu() {
+        burgerButton.click();
+        return this;
+    }
+
+    @Step("Открытие раздела \"Все услуги\"")
+    public MainPage openAllProvider() {
+        allProvider.click();
+        return this;
+    }
+
+    @Step("Открытие раздела \"Все вакансии\"")
+    public MainPage openAllVacancies() {
+        allVacancies.click();
+        return this;
+    }
+
+    @Step("Нажатие кнопки навигации {key} в бургер-меню")
+    public MainPage openElementNavigationInBurgerMenu(String key) {
+        menuNavigation.$(byText(key)).click();
+        return this;
+    }
+
+    @Step("Проверка наличия ссылки {key} в логотипе компании")
+    public void checkLinkHeaderLogo(String key) {
+        headerLogo.shouldHave(href(key));
+    }
+
+    @Step("Проверка заголовка {key} в открывшемся разделе")
+    public void checkHeaderOpenPage(String key) {
+        bigHeading.shouldHave(text(key));
+    }
+
+    @Step("Проверка названий элементов навигации бургер-меню")
+    public void checkNameElement(String key) {
+        menuNavigation.shouldHave(text(key));
+    }
+
+    @Step("Проверка совпадения Url текущей страницы с {key}")
+    public void checkUrl(String key) {
+        webdriver().shouldHave(currentFrameUrl(key));
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -21,7 +22,7 @@ public class TestBase {
         Configuration.browserVersion = System.getProperty("version","121");
 
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.remote = "https://user1:1234@"+System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
+        Configuration.remote = "https://user1:1234@"+System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -33,10 +34,10 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
 }
